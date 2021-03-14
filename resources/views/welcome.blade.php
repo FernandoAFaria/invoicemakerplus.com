ecdd9c2c178f4f5f5140fea3eb8431eefd96a0c3
fernandofaria <fernandoafaria@yahoo.com>
2021-03-13 23:54:43 -0500

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,7 +63,7 @@ $connection = mysqli_connect('localhost','root','Mustang1!','thefrontenddevdb');
 $users = mysqli_query($connection,'select * from contributor_info');
 
 $userObj = $users -> fetch_object();
-print_r($userObj);
+print_r($userObj[1]);
 ?>
 
 
