68793e1235cd86543aa06af1aaa2ef5b693c664b
fernandofaria <fernandoafaria@yahoo.com>
2021-03-14 00:25:43 -0500

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,23 +53,7 @@
 
 <?php
 
-
-    $connection = mysqli_connect('localhost','root','Mustang1!','thefrontenddevdb');
-
-    $users = mysqli_query($connection,'select * from contributor_info');
-
-    while($row = mysqli_fetch_assoc($users)){
-
-        ?>
-
-        <div>
-            <h5><?php echo $row['first_name'] ?> </h5>
-            <img src="<?php echo $row['picture'] ?>" />
-        </div>
-
-
-<?php
-    }
+include('app/helpers.php')
 ?>
 
 
