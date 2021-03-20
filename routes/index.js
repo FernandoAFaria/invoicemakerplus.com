902a968d4eaf9b1c4ea5bbd26a3971fc562364a8
fernandofaria <fernandoafaria@yahoo.com>
2021-03-20 09:27:09 -0400

    change

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Invoice Maker Plus' });
+  res.render('index', { title: 'Invoice Maker Plus', pageTitle: 'Invoice Maker Plus' });
 });
 
 module.exports = router;
